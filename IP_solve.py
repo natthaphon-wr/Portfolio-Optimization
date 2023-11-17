5af3eee8c1888f4b4cf096e8552577dcc9a3cfd1
natthaphon-wr <natthaphon.win@gmail.com>
2023-11-17 11:17:43 +0700

    remove some symbols and implement ip on multiobjective

--- IP_solve.py
+++ IP_solve.py
@@ -35,11 +35,13 @@ def solver_single(Symbol, P, ER, R, L, budget):
 
   # Define the constraints
   model += lpSum(x[i] * P[i] for i in assets) <= budget, "Budget_Constraint"
+  model += lpSum(x[i] * P[i] * R[i] for i in assets)/budget <= 0.2, "Risk_Constraint"
   for i in assets:
     model += (x[i] * P[i])/budget <= 0.7, f"Diversity_{i}_Constraint"
 
   # Solve the optimization problem
   model.solve(PULP_CBC_CMD(msg=False))
+  # model.solve()
 
   # Print the solution status
   # print("Status:", model.status)
@@ -49,11 +51,11 @@ def solver_single(Symbol, P, ER, R, L, budget):
   #   print(f"{name}: {value(constraint)}")
 
   # Result: Symbol and Number of shares to invest
-  result_col = ['Symbol', 'Price', 'NumberShares']
+  result_col = ['Symbol', 'Price', 'NumberShares', 'Expected Return', 'Risk']
   result_df = pd.DataFrame(columns=result_col)
   for i in assets:
     if value(x[i]) > 0:
-      new_data = pd.DataFrame([[Symbol[i], P[i], value(x[i])]], columns=result_col)
+      new_data = pd.DataFrame([[Symbol[i], P[i], value(x[i]), ER[i], R[i]]], columns=result_col)
       result_df = pd.concat([result_df, new_data], ignore_index=True)
 
   # Print the optimized total return
@@ -76,28 +78,30 @@ def main_single(path, n, budget):
 #   Medium problem is using 20 symbols that selected symbols with top20 liquidity 
 #   Smallest problem is using 10 symbols that selected symbols with top10 liquidity 
 def main():
-  path = 'Symbols27.csv'
+  path = 'Symbols24.csv'
 
   # Small problem
-  result_10, totalInvest_10, optReturn_10 = main_single(path, 10, 1000000)
-  print('\nSmall problem with 10 considered symbols')
-  print(result_10)
-  print('Total Investment: ', totalInvest_10)
-  print("Optimized Total Return:", optReturn_10)
+  result_12, totalInvest_12, optReturn_12 = main_single(path, 12, 1000000)
+  print('\nSmall problem with 12 considered symbols')
+  print(result_12)
+  print('Total Investment: ', totalInvest_12)
+  print("Optimized Total Return:", optReturn_12)
 
-  # Medium problem
-  result_20, totalInvest_20, optReturn_20 = main_single(path, 20, 1000000)
-  print('\nMedium problem with 20 considered symbols')
-  print(result_20)
-  print('Total Investment: ', totalInvest_20)
-  print("Optimized Total Return:", optReturn_20)
+  # # Medium problem
+  # result_15, totalInvest_15, optReturn_15 = main_single(path, 15, 1000000)
+  # print('\nMedium problem with 20 considered symbols')
+  # print(result_15)
+  # print('Total Investment: ', totalInvest_15)
+  # print("Optimized Total Return:", optReturn_15)
 
   # Large problem
-  result_27, totalInvest_27, optReturn_27 = main_single(path, 27, 1000000)
-  print('\nLarge problem with 27 considered symbols')
-  print(result_27)
-  print('Total Investment: ', totalInvest_27)
-  print("Optimized Total Return:", optReturn_27)
+  result_24, totalInvest_24, optReturn_24 = main_single(path, 24, 1000000)
+  print('\nLarge problem with 24 considered symbols')
+  print(result_24)
+  print('Total Investment: ', totalInvest_24)
+  print("Optimized Total Return:", optReturn_24)
 
 
 main()
+# top_rows = data_top('Symbols27.csv', 27)
+
--- pre_eliminate.py
+++ pre_eliminate.py
@@ -5,14 +5,39 @@ csv_file_path = 'opt_dataset.csv'
 
 df = pd.read_csv(csv_file_path)
 
-condition = (df['Liquidity'] >= 0.5) & (df['Liquidity'] <= 0.8)
+condition = (df['Liquidity'] >= 0.5) & (df['Liquidity'] <= 0.8) & (df['Expected Return'] <= 0.3) & (df['Lastest Price'] <= 10000) 
 filtered_df = df[condition]
 df_sorted = filtered_df.sort_values(by='Liquidity', ascending=False)
+
+# Plot histrogram price
+plt.hist(df_sorted['Lastest Price'])
+plt.xlabel('Price')
+plt.ylabel('Number of Stock Symbols')
+plt.title('Histrogram of Price in Data')
+plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+plt.show()
+
+# Plot histrogram expected return
+plt.hist(df_sorted['Expected Return'])
+plt.xlabel('Expected Return')
+plt.ylabel('Number of Stock Symbols')
+plt.title('Histrogram of Expected Returna in Data')
+plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+plt.show()
+
+# Plot histrogram risk
+plt.hist(df_sorted['Risk'])
+plt.xlabel('Risk')
+plt.ylabel('Number of Stock Symbols')
+plt.title('Histrogram of Risk in Data')
+plt.gca().yaxis.set_major_locator(plt.MaxNLocator(integer=True))
+plt.show()
+
 print("Filtered Data:")
 print(df_sorted)
 print("Number of considering stock: ", len(df_sorted))
 
 # Save new df to csv file
-file_path = 'Symbols27.csv'  # Set the file path and name
-df_sorted.to_csv(file_path, index=False)
+# file_path = 'Symbols24.csv'  # Set the file path and name
+# df_sorted.to_csv(file_path, index=False)
 
