23ba505abe5fa631a4791d4baa66bc6fa4c427dc
natthaphon-wr <natthaphon.win@gmail.com>
2023-11-20 18:04:15 +0700

    update ip and initial ga

--- IP_solve.py
+++ IP_solve.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from pulp import LpProblem, LpMaximize, LpVariable, lpSum, value
 from pulp.apis import PULP_CBC_CMD
 
-
 # Select topN Liquidity from dataset
 def data_top(path, n):
   df_all = pd.read_csv(path)
@@ -21,13 +20,13 @@ def df2lists(df):
   return Symbol, P, ER, R, L
 
 # Solving the problem with pulp
-def solver_single(Symbol, P, ER, R, L, budget):
+def solver_single(Symbol, P, ER, R, L, budget, multi=False, risk=0):
   # Create the optimization problem
   model = LpProblem(name="Portfolio_Optimization", sense=LpMaximize)
 
   # Define decision variables (integer variables for asset selection)
   assets = range(len(Symbol))
-  x = LpVariable.dicts("Asset", assets, lowBound=0, upBound=1000, cat='Integer')
+  x = LpVariable.dicts("Asset", assets, lowBound=0, upBound=999, cat='Integer')
   y = LpVariable.dicts("Asset_select", assets, cat='Binary')
 
   # Define the objective function
@@ -35,7 +34,8 @@ def solver_single(Symbol, P, ER, R, L, budget):
 
   # Define the constraints
   model += lpSum(x[i] * P[i] for i in assets) <= budget, "Budget_Constraint"
-  model += lpSum(x[i] * P[i] * R[i] for i in assets)/budget <= 0.2, "Risk_Constraint"
+  if multi:
+    model += lpSum(x[i] * P[i] * R[i] for i in assets)/budget <= risk, "Risk_Constraint"
   for i in assets:
     model += (x[i] * P[i])/budget <= 0.7, f"Diversity_{i}_Constraint"
 
@@ -64,10 +64,10 @@ def solver_single(Symbol, P, ER, R, L, budget):
   return result_df, optimized_return
 
 # Read and select top data
-def main_single(path, n, budget):
+def main_opt(path, n, budget):
   df = data_top(path, n)
   Symbol, P, ER, R, L = df2lists(df)
-  result_df, optimized_return = solver_single(Symbol, P, ER, R, L, budget)
+  result_df, optimized_return = solver_single(Symbol, P, ER, R, L, budget, multi=True, risk=0.075)
   total_invest = (result_df['Price'] * result_df['NumberShares']).sum()
 
   return result_df, total_invest, optimized_return
@@ -81,21 +81,14 @@ def main():
   path = 'Symbols24.csv'
 
   # Small problem
-  result_12, totalInvest_12, optReturn_12 = main_single(path, 12, 1000000)
+  result_12, totalInvest_12, optReturn_12 = main_opt(path, 12, 1000000)
   print('\nSmall problem with 12 considered symbols')
   print(result_12)
   print('Total Investment: ', totalInvest_12)
   print("Optimized Total Return:", optReturn_12)
 
-  # # Medium problem
-  # result_15, totalInvest_15, optReturn_15 = main_single(path, 15, 1000000)
-  # print('\nMedium problem with 20 considered symbols')
-  # print(result_15)
-  # print('Total Investment: ', totalInvest_15)
-  # print("Optimized Total Return:", optReturn_15)
-
   # Large problem
-  result_24, totalInvest_24, optReturn_24 = main_single(path, 24, 1000000)
+  result_24, totalInvest_24, optReturn_24 = main_opt(path, 24, 1000000)
   print('\nLarge problem with 24 considered symbols')
   print(result_24)
   print('Total Investment: ', totalInvest_24)
@@ -103,5 +96,3 @@ def main():
 
 
 main()
-# top_rows = data_top('Symbols27.csv', 27)
-
